754a8acc52c8f19c2d9e6d99556c1a5653e9385c
liyongsea <lijia0765@gmail.com>
2014-05-20 10:52:30 +0200

    L1

--- code/src/L1study.m
+++ code/src/L1study.m
@@ -1,7 +1,7 @@
 clear all
 close all
 %%
-lambda_list=0:1:15;
+lambda_list=0:0.5:15;
 it=1;
 for lambda=lambda_list
     
@@ -10,18 +10,21 @@ thresh=@(c,lambda)sign(c).*max(abs(c)-lambda,0);
 h=@(c,lambda)0.5*c.^2.*(abs(c)<=lambda)+(abs(c)>lambda).*(0.5*lambda^2+lambda.*(abs(c)-lambda));
 sq=@(c)0.5*c.^2;
 c=[-5:0.01:5];
-% figure(),plot(c,h(c,lambda))
-hold on, plot(c,sq(c),'-r')
+% hold on, plot(c,h(c,lambda),'Color',[it/length(lambda_list), 0 , 1-it/length(lambda_list)],'LineWidth',2)
+% hold on, plot(c,sq(c),'-r')
 
 %%
-
+% end
+% title('the function g for different lambda')
+%%
 x1=5;
-x2=[-5:0.01:15];
+x2=[-15:0.01:30];
 dist=@(x1,x2,lambda)2*h((x1+x2)/2,lambda/2)-h(x1,lambda)-h(x2,lambda)+0.25*(x1-x2).^2;
 figure(3),hold on, plot(x2,dist(x1,x2,lambda),'LineWidth',2,'Color',[it/length(lambda_list) 0 1-it/length(lambda_list)])
 it=it+1;
 end
-hold on, plot(x2,0.25*(x1-x2).^2,'LineWidth',2,'Color',[1 0 0 ])
+xlabel('x2'),ylabel('-log Pg (x1, x2)'),title('similarity criterion for x1=5')
+% hold on, plot(x2,0.25*(x1-x2).^2,'LineWidth',2,'Color',[1 0 0 ])
 %%
 ss=[6000,3000];
 A=10*randn(ss);
